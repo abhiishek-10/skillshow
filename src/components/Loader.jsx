2d0d515fd81612a44577e2d5ecc6aa2e6ff1c610
Abhishek P <abhishek.patel@yudiz.com>
2023-05-31 13:19:33 +0530

    Add: new build

---
+++ src/components/Loader.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import "./main.css"
+import loading from '../lotties/loading.json';
+
+const Loader = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: loading,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+    useEffect(() => {
+        window.onload = () => {
+            const preloader = document.querySelector(".preloader");
+            console.log("done");
+            setTimeout(() => {
+                console.log("done 2");
+                preloader.style.opacity = 0;
+            }, 3000);
+        }
+    });
+    return (<div className="preloader">
+
+
+
+    </div>)
+    // which have your preloader html and having class is `.preloader`
+};
+
+
+export default Loader
